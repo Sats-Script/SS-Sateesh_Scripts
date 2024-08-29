4e433ab08f29228a4cc091e12654aa36a4a0cfc8
Sateesh <satish.matapathi@gmail.com>
2024-08-29 11:24:01 +0530

    Converse dialogues

---
+++ convers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+I=Sateesh
+U=Harish
+
+echo "$I : Hey $U how are you bro"
+echo "$U  : hey $I i am fine bro "
+echo "$I : i heard you upgraded with new skill is it true Mr $U"
+echo "$U : Yeah true . you do it yourself mr $I"
