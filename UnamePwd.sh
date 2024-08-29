f4049264fde567e0af8e62da1461e331668eefc1
Sateesh <satish.matapathi@gmail.com>
2024-08-29 12:19:30 +0530

    UserandPassword

---
+++ UnamePwd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#here i am writing Username and password authentication
+echo "Please enter your name"
+read Uname
+echo "Enter your pwd"
+read Pwd
+echo "Entered Name and it Password is $Uname  ; $Pwd"
