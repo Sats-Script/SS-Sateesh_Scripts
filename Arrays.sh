d8b0cc73b80a8d1ea1d85422a4a6e621cfc50b70
Sateesh <satish.matapathi@gmail.com>
2024-08-29 15:34:56 +0530

    UserandPassword

--- Arrays.sh
+++ Arrays.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 #here i am writing small Arrays
-Fruits=$("1" "2" "3" )
-echo "Please enter first fruit name"
-echo "Please enter Second fruit name"
-echo "Please enter Third fruit name"
+x=$1 
+y=$2
+z=$3
+Fruits=("$x" "$y" "$z")
+# echo "Please enter first fruit name"
+# read 
+# echo "Please enter Second fruit name"
+# echo "Please enter Third fruit name"
 
 Echo "entered fruit names are ${Fruits[@]}"
