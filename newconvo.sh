335c959c03d08c8038025a1dc324877d4c084f56
Sateesh <satish.matapathi@gmail.com>
2024-08-29 11:38:50 +0530

    New one

--- newconvo.sh
+++ newconvo.sh
@@ -2,8 +2,9 @@
 
 I=$1
 U=$2
+R="\e[31m"
 
-echo "${I} : Hey $U how are you bro"
+$R echo "${I} : Hey $U how are you bro"
 echo "${U}  : hey $I i am fine bro "
 echo "$I : i heard you upgraded with new skill is it true Mr $U"
 echo "$U : Yeah true . you do it yourself mr ${I}"
