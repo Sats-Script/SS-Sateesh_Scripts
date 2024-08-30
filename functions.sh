545ca6c6f01e06fd473ddb509f6cb4f6a414c8bd
Sateesh <satish.matapathi@gmail.com>
2024-08-30 12:04:10 +0530

    Functions test

--- functions.sh
+++ functions.sh
@@ -4,7 +4,24 @@ Uid=$(id -u)
 
 if [ $Uid -ne 0 ] 
 then
-echo "Please run script as administrator $Uid "
+echo "Please run script as Sudo user ; current userid is :$Uid "
 else
-echo "Script can run in Admin mode now $Uid" 
+echo "Script is running now as sudo user : enter package name $1" 
+package=$1
 fi
+
+validate(){
+    if [ $1 -ne 0 ]
+    then
+    echo "$2  installation begins now"
+    installate
+    else
+    echo "$2 is already installed "
+}
+
+installate(){
+    dnf install $package -y
+}
+
+dnf list installed $package
+validate $? "$package "
