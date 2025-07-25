ad9b52fab2fb4c3a57a9aac6824c1ca115cf1445
Sateesh <satish.matapathi@gmail.com>
2025-07-25 14:18:18 +0530

    Delete UnamePwd.sh

--- UnamePwd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#here i am writing Username and password authentication
-echo "Please enter your name"
-read -s Uname
-echo "Enter your pwd"
-read -s Pwd
-echo "Entered Name and it Password is $Uname  ; $Pwd"
